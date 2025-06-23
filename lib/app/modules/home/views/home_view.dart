42f7190ad5db40a866923779df199389885d6f50
TomiSakae <tomisakaeap3@gmail.com>
2025-06-23 17:53:11 +0700

    update reponsive nav

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../controllers/home_controller.dart';
 import '../../library/views/library_view.dart';
@@ -24,52 +26,143 @@ class HomeView extends GetView<HomeController> {
           SettingsView(),
         ],
       )),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withOpacity(.1),
-            )
-          ],
+      bottomNavigationBar: _buildResponsiveNavBar(),
+    );
+  }
+
+  Widget _buildResponsiveNavBar() {
+    return ResponsiveBreakpoints.of(Get.context!).isMobile
+        ? _buildMobileNavBar()
+        : _buildTabletNavBar();
+  }
+
+  Widget _buildMobileNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20.r,
+            color: Colors.black.withOpacity(0.1),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Container(
+          height: 60.h,
+          padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+          child: Obx(() => Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(0, Iconsax.book, 'Thư viện', true),
+              _buildNavItem(1, Iconsax.book_1, 'Đọc', true),
+              _buildNavItem(2, Iconsax.clock, 'Lịch sử', true),
+              _buildNavItem(3, Iconsax.setting_2, 'Cài đặt', true),
+            ],
+          )),
         ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-            child: Obx(() => GNav(
-              rippleColor: Colors.grey[300]!,
-              hoverColor: Colors.grey[100]!,
-              gap: 8,
-              activeColor: Colors.black,
-              iconSize: 24,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-              duration: const Duration(milliseconds: 400),
-              tabBackgroundColor: Colors.grey[100]!,
-              color: Colors.black,
-              tabs: const [
-                GButton(
-                  icon: Iconsax.book,
-                  text: 'Thư viện',
+      ),
+    );
+  }
+
+  Widget _buildTabletNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20.r,
+            color: Colors.black.withOpacity(0.1),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Container(
+          height: 70.h,
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+          child: Obx(() => GNav(
+            rippleColor: Colors.grey[300]!,
+            hoverColor: Colors.grey[100]!,
+            gap: 8.w,
+            activeColor: Colors.black,
+            iconSize: 24.r,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+            duration: const Duration(milliseconds: 400),
+            tabBackgroundColor: Colors.grey[100]!,
+            color: Colors.black,
+            tabs: [
+              GButton(
+                icon: Iconsax.book,
+                text: 'Thư viện',
+                textStyle: TextStyle(fontSize: 12.sp),
+              ),
+              GButton(
+                icon: Iconsax.book_1,
+                text: 'Đọc truyện',
+                textStyle: TextStyle(fontSize: 12.sp),
+              ),
+              GButton(
+                icon: Iconsax.clock,
+                text: 'Lịch sử',
+                textStyle: TextStyle(fontSize: 12.sp),
+              ),
+              GButton(
+                icon: Iconsax.setting_2,
+                text: 'Cài đặt',
+                textStyle: TextStyle(fontSize: 12.sp),
+              ),
+            ],
+            selectedIndex: controller.currentIndex.value,
+            onTabChange: (index) {
+              controller.changeTabIndex(index);
+            },
+          )),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(int index, IconData icon, String label, bool isMobile) {
+    final isSelected = controller.currentIndex.value == index;
+
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => controller.changeTabIndex(index),
+        behavior: HitTestBehavior.opaque,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          padding: EdgeInsets.symmetric(
+            horizontal: isMobile ? 4.w : 8.w,
+            vertical: isMobile ? 6.h : 8.h,
+          ),
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.blue.withOpacity(0.1) : Colors.transparent,
+            borderRadius: BorderRadius.circular(8.r),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: isMobile ? 18.r : 20.r,
+                color: isSelected ? Colors.blue : Colors.grey[600],
+              ),
+              SizedBox(height: 2.h),
+              Flexible(
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: isMobile ? 9.sp : 10.sp,
+                    color: isSelected ? Colors.blue : Colors.grey[600],
+                    fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                GButton(
-                  icon: Iconsax.book_1,
-                  text: 'Đọc truyện',
-                ),
-                GButton(
-                  icon: Iconsax.clock,
-                  text: 'Lịch sử',
-                ),
-                GButton(
-                  icon: Iconsax.setting_2,
-                  text: 'Cài đặt',
-                ),
-              ],
-              selectedIndex: controller.currentIndex.value,
-              onTabChange: (index) {
-                controller.changeTabIndex(index);
-              },
-            )),
+              ),
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -18,33 +20,51 @@ class LinkStoryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'LinkStory',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          elevation: 0,
-        ),
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          elevation: 0,
-        ),
-      ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812), // iPhone X design size
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'LinkStory',
+          builder: (context, child) {
+            return ResponsiveBreakpoints.builder(
+              child: child!,
+              breakpoints: [
+                const Breakpoint(start: 0, end: 450, name: MOBILE),
+                const Breakpoint(start: 451, end: 800, name: TABLET),
+                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+              ],
+            );
+          },
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blue,
+              brightness: Brightness.light,
+            ),
+            useMaterial3: true,
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              elevation: 0,
+            ),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blue,
+              brightness: Brightness.dark,
+            ),
+            useMaterial3: true,
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              elevation: 0,
+            ),
+          ),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
